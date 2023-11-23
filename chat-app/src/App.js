0c6e7ffbf5d48401c5bd115ddef9b0d3e1707bf5
abbygup18 <125712345+abbygup18@users.noreply.github.com>
2023-11-23 16:55:37 -0600

    Added a textbox

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,10 +1,43 @@
+// App.js
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState('');
+
+  const handleSendMessage = () => {
+    if (newMessage.trim() !== '') {
+      setMessages([...messages, { text: newMessage, sender: 'user' }]);
+      setNewMessage('');
+      // In a real app, you might send the message to a server here
+    }
+  };
+
   return (
-    <div className="App">
-
+    <div className="app">
+      <div className="chat-container">
+        <div className="messages">
+          {messages.map((message, index) => (
+            <div
+              key={index}
+              className={`message ${message.sender === 'user' ? 'user' : 'bot'}`}
+            >
+              {message.text}
+            </div>
+          ))}
+        </div>
+        <div className="input-container">
+          <input
+            type="text"
+            placeholder="Type your message..."
+            value={newMessage}
+            onChange={(e) => setNewMessage(e.target.value)}
+          />
+          <button onClick={handleSendMessage}>Send</button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
